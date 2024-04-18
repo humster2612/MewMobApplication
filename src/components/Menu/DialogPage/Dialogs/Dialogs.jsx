7aad042e47855b77825a423062008241f01f6e0e
Victoria <w67964@student.wsiz.edu.pl>
2024-04-18 12:57:29 +0200

    detailed DialogsPage , added button and field for messages and people's avatars

--- src/components/Menu/DialogPage/Dialogs/Dialogs.jsx
+++ src/components/Menu/DialogPage/Dialogs/Dialogs.jsx
@@ -2,25 +2,62 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import { FaCheck } from "react-icons/fa";
+
+
 
 const Dialogs = (props) => {
-    const dialogsElements = props.state.dialogsData.map(d => <DialogItem name={d.name} id={d.id} />) 
-    const messagesElements = props.state.messageData.map(m => <Message message={m.message} id={m.id} />)
-
-    return (
-        <div className={s.containerthree}>
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                {dialogsElements}
-            </div>
-            <div className={s.messages}>
-                {messagesElements}
+    const dialogsElements = props.state.dialogsData.map(d => (
+        <div className={s.dialogItem}>
+            <div className={s.avatarAndName}>
+                <img
+                    alt=""
+                    className={s.avatar}
+                    src={d.avatarUrl} 
+                />
+                <DialogItem name={d.name} id={d.id} />
             </div>
         </div>
+    ));
 
+    const messagesElements = props.state.messageData.map(m => <Message message={m.message} id={m.id} />);
+
+        let NewMessage=React.createRef();
+
+
+    const SendMessage = () =>{
+        let text = NewMessage.current.value;
+        alert (text);
+
+    }
+
+    return (
+        <div className={s.containerthree}> 
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    {dialogsElements}
+                </div>
+                <div className={s.messages}>
+                    {messagesElements}
+                    <div className={s.postBlock}>
+
+                        <div className={s.fieldformesssage}>
+                      
+                              
+                              <input className={s.input} type="text" ref={NewMessage} placeholder="Enter your comment" />
+            <button className={s.addButton} onClick={SendMessage}>Send Message</button>
+                        </div>
+     
+
+         
+        </div>
+                </div>
+            </div>
         </div>
     );
 };
+
+
 
 
 export default Dialogs;
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,10 +1,10 @@
 let state = {
 
             dialogsData:[
-                    {id: '1', name: 'Kubik'},
-                    {id: '2', name: 'Czubi'},
-                    {id: '3', name: 'Burulka'},
-                    {id: '4', name: 'Bobik'}
+                    {id: '1', name: 'Kubik', avatarUrl:'https://www.radiustheme.com/demo/wordpress/themes/cirkle/wp-content/uploads/avatars/1/60af1abf02c8c-bpfull.jpg'},
+                    {id: '2', name: 'Czubi', avatarUrl:'https://www.radiustheme.com/demo/wordpress/themes/cirkle/wp-content/uploads/avatars/19/60af144800120-bpfull.jpg'},
+                    {id: '3', name: 'Burulka',avatarUrl:'https://www.radiustheme.com/demo/wordpress/themes/cirkle/wp-content/uploads/avatars/26/60af431b91a0a-bpfull.jpg'},
+                    {id: '4', name: 'Bobik',avatarUrl:'https://www.radiustheme.com/demo/wordpress/themes/cirkle/wp-content/uploads/avatars/1/60af1abf02c8c-bpfull.jpg'}
                 ],
 
                 messageData: [
