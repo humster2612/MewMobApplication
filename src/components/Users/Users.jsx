1171a5cd4e934e6934795464d22df60d23253ba6
Victoria <w67964@student.wsiz.edu.pl>
2024-05-29 13:06:04 +0200

    added class component in Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,48 +1,66 @@
+//это если что классовая компонента 
 import React, { useEffect } from "react";
 import styles from './users.module.css';
 import axios from "axios";
 import userPhoto from '../../assets/images/60af1abf02c8c-bpfull.jpg';
 
-
-let Users = (props) => {
-  useEffect(() => {
-    if (props.users.length === 0) { // Проверяем, что пользователи еще не загружены
-     
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response =>{
-        props.setUsers(response.data.items);        
-      } )
+class Users extends React.Component{
+    componentDidMount(){
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response =>{
+      this.props.setUsers(response.data.items);        
+      })
     }
-  }, []); // Пустой массив зависимостей означает, что useEffect вызывается только при монтировании компонента
+    
+    render(){
+      return (
+          <div>
+      
+          <button onClick={this.getUsers}>Get Users</button>
+      
+            {this.props.users && this.props.users.map(u => (
+              <div key={u.id}>
+                <span>
+                  <div>
+                    <img src={u.photos.small !=null ? u.photos.small:userPhoto} className={styles.userPhoto} />
+                  </div>
+                  <div>
+                    {u.followed
+                      ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                      : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+                  </div>
+                </span>
+                <span>
+                  <span>
+                    <div>{u.fullName}</div>
+                    <div>{u.status}</div>
+                  </span>
+                  <span>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
+                  </span>
+                </span>
+              </div>
+            ))}
+          </div>
+      )
+  }
+}
+                
 
-  return (
-    <div>
-      {props.users && props.users.map(u => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img src={u.photos.small !=null ? u.photos.small:userPhoto} className={styles.userPhoto} />
-            </div>
-            <div>
-              {u.followed
-                ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{u.fullName}</div>
-              <div>{u.status}</div>
-            </span>
-            <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
-            </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+      
+       
+
+
+
+// Пустой массив зависимостей означает, что useEffect вызывается только при монтировании компонента
+
+// useEffect(() => {
+// getUsers();
+// }, []);
+
+
+    
+
 
 export default Users;
